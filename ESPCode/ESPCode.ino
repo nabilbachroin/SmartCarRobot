a5672bb263255f3e3d8e1bde3c30ed3b201b1cd6
nabilbachroin <abiel769@gmail.com>
2023-12-20 00:11:48 +0800

    prepared for Firebase database

--- ESPCode/ESPCode.ino
+++ ESPCode/ESPCode.ino
@@ -1,5 +1,17 @@
 #include <Arduino.h>
+#include <WiFi.h>
 #include <Bonezegei_DHT11.h>
+#include <Firebase_ESP_Client.h>
+#include "addons/TokenHelper.h"
+#include "addons/RTDBHelper.h"
+
+#define WIFI_SSID "Bachroin"
+#define WIFI_PASSWORD "masnabilganteng"
+#define API_KEY "AIzaSyB2TfOLtjRMxYWD5qA0TqWjH0ICUYg_6EA"
+#define DATABASE_URL "https://smartcarrobot-microsensors-default-rtdb.asia-southeast1.firebasedatabase.app/"
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
 
 const int trigPin1 = 12; 
 const int echoPin1 = 13;
@@ -40,6 +52,29 @@ float getDistance(int trigPin, int echoPin) {
 
 void setup() {
   Serial.begin(9600);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+  config.api_key = API_KEY;
+  config.database_url = DATABASE_URL;
+  if (Firebase.signUp(&config, &auth, "", "")){
+    Serial.println("ok");
+    signupOK = true;
+  }
+  else{
+    Serial.printf("%s\n", config.signer.signupError.message.c_str());
+  }
+  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
+
 
   pinMode(trigPin1, OUTPUT);
   pinMode(echoPin1, INPUT);
